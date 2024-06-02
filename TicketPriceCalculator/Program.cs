3f0b01f3787db0f11c6d4601fc030f01c6bde42d
Kingsley Kwame Dogbey <king.dogbey777@gmail.com>
2024-06-02 00:20:58 +0000

    Second Commit

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class TicketPriceCalculator{
+  static void Main()
+  {
+    Console.WriteLine("Enter your Age");
+    int age= Convert.ToInt32(Console.ReadLine());
+
+    if (age >=65)
+    {
+        Console.WriteLine("The price of your ticket is GHC3");
+    }
+    else if (age <=12)
+    {
+        Console.WriteLine("The price of your ticket is GHC3");
+    }
+    else
+    {
+        Console.WriteLine("The price of your ticket is GHC10");
+    }
+  }
+}
